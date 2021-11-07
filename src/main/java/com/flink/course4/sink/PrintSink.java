d3d347826672294115e9a0b96a076fdbb8c62ef5
dragon <893564413@qq.com>
2021-11-07 15:46:49 +0800

    add:flink 源码的解析和基础实现

---
+++ src/main/java/com/flink/course4/sink/PrintSink.java
@@ -0,0 +1,12 @@
+package com.flink.course4.sink;
+
+/**
+ * @description:
+ * @author: 龙辉辉
+ * @create: 2021-11-07 15:42
+ */
+public class PrintSink {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/main/java/com/flink/course4/transform/MapTransform.java
@@ -0,0 +1,9 @@
+package com.flink.course4.transform;
+
+/**
+ * @description:
+ * @author: 龙辉辉
+ * @create: 2021-11-07 15:46
+ */
+public class MapTransform {
+}
